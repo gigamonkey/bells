345583705ae80404f05d1a9a746f12296ec52850
Peter Seibel <peter@gigamonkeys.com>
2024-04-24 19:48:14 -0700

    Get newer version of my dom library.

--- dom.js
+++ dom.js
@@ -8,9 +8,15 @@ const tagPattern = /^<(\w+)>$/;
  * '<p>' it creates an element of that type. Otherwise it queries the document
  * using the argument as a selector.
  */
-const $ = (q) => {
+const $ = (q, content) => {
   const tag = q.match(tagPattern)?.[1];
-  return tag ? document.createElement(tag) : document.querySelector(q);
+  if (tag) {
+    const e = document.createElement(tag);
+    if (content !== undefined) e.append(content);
+    return e;
+  } else {
+    return document.querySelector(q);
+  }
 };
 
 /*
@@ -34,4 +40,44 @@ const html = (html) => {
   return children.length == 1 ? children[0] : children;
 };
 
-export { $, $$, text, html };
+/*
+ * Get an object containing all DOM objects with an id.
+ */
+const byId = () => Object.fromEntries([...document.querySelectorAll('[id]')].map(e => [e.id, e]));
+
+
+/*
+ * Decorate a DOM element with a CSS class.
+ */
+const withClass = (className, e) => {
+  e.className = className;
+  return e;
+};
+
+/*
+ * Make an A element with a give text and href and an optional target.
+ */
+const a = (text, href, target) => {
+  const e = $('<a>', text);
+  e.setAttribute('href', href);
+  if (target) e.setAttribute('target', target);
+  return e;
+};
+
+/*
+ * Make a named Bootstrap icon.
+ */
+const icon = (name) => {
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+  svg.classList.add('bi');
+  use.setAttributeNS(
+    'http://www.w3.org/1999/xlink',
+    'xlink:href',
+    `/img/bootstrap-icons.svg#${name}`,
+  );
+  svg.append(use);
+  return svg;
+};
+
+export { $, $$, a, byId, html, icon, text, withClass };
