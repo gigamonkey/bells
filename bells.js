33cfed25841397bf3536ab5040e394cc11fc2424
Peter Seibel <peter@gigamonkeys.com>
2024-05-18 18:30:27 -0700

    Guessing at teacher start of 2024-25 year.

--- bells.js
+++ bells.js
@@ -13,7 +13,7 @@ const setOffset = (year, month, date, hour = 12, min = 0, second = 0) => {
   offset = new Date(year, month - 1, date, hour, min, second).getTime() - new Date().getTime();
 };
 
-//setOffset(2024, 6, 3, 8, 0, 0);
+//setOffset(2024, 8, 12, 8, 2, 55 );
 
 // Always use this to get the "current" time to ease testing.
 const now = () => {
