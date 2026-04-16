3af96fae7884cc6a71209cbd4e26f6eb4f85b6f4
Peter Seibel <peter@gigamonkeys.com>
2026-04-16 13:13:26 -0700

    Update publish script.

--- publish.sh
+++ publish.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 set -eou pipefail
-set -x
 
 dir=$(basename "$(pwd)")
 sha=$(git log --pretty=tformat:%H -1);
 # Hacked from canonical publish.sh file to use custom webdir.
 webdir=~/web/www.gigamonkeys.com/misc/bhs/
 
+echo "Copying files to $webdir"
 mkdir -p "$webdir"
 cp -R "$@" $webdir
 cd $webdir
+
+echo "Git adding."
 git add .
-git commit -m "Publish $dir $sha" .
-git push
+
+if git diff --cached --quiet .; then
+  echo "Nothing to publish — working tree unchanged from last publish."
+  exit 0
+else
+    echo "Committing and pushing."
+    git commit -m "Publish $dir $sha" .
+    git push
+fi
